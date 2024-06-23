96116ccfa81772bad81a2dcf2660a8720979edd8
Vladimir-Strelec <vovik050github@gmail.com>
2024-06-23 23:54:10 +0200

    Add project with build Maven

--- Project/src/designpatterns/authsystem/UserExistsHandler.java
+++ Project/src/designpatterns/authsystem/UserExistsHandler.java
@@ -1,13 +1,2 @@
-package designpatterns.authsystem;
-
-public class UserExistsHandler extends Handler{
-
-    public UserExistsHandler(DatabaseClass databaseClass) {
-        super(databaseClass);
-    }
-
-    @Override
-    public boolean handle(String name, String pass) {
-        return this.databaseClass.isValidUser(name);
-    }
+package designpatterns.authsystem;public class UserExistsHandler {
 }
--- Project/src/designpatterns/authsystem/ValidPasswordHandler.java
+++ Project/src/designpatterns/authsystem/ValidPasswordHandler.java
@@ -1,12 +1,2 @@
-package designpatterns.authsystem;
-
-public class ValidPasswordHandler extends Handler{
-    public ValidPasswordHandler(DatabaseClass databaseClass) {
-        super(databaseClass);
-    }
-
-    @Override
-    protected boolean handle(String name, String pass) {
-        return databaseClass.isValidUser(name, pass);
-    }
+package designpatterns.authsystem;public class ValidPasswordHandler {
 }
